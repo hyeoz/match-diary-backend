fce5bbe00e0234edd6eddbb27d3c39a52861de9a
hyeoz <jenny970203@gmail.com>
2024-05-05 22:38:17 +0900

    init: initial commit

---
+++ src/api/schedule-2024/controllers/schedule-2024.ts
@@ -0,0 +1,7 @@
+/**
+ * schedule-2024 controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::schedule-2024.schedule-2024');
---
+++ src/api/schedule-2024/routes/schedule-2024.ts
@@ -0,0 +1,7 @@
+/**
+ * schedule-2024 router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::schedule-2024.schedule-2024');
